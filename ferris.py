5007cc047819de1c3e2ac3931d9d1d9b08dfe34a
Tobi Schäfer <interface@p3k.org>
2012-05-19 13:35:35 +0200

    Initial commit

---
+++ ferris.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2011 Tobi Schäfer.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import simplejson as json
+
+from datetime import datetime, timedelta
+from urlparse import urlparse
+
+from google.appengine.api import memcache
+
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class Referrer(db.Model):
+   group = db.StringProperty()
+   hits = db.IntegerProperty(default = 0)
+   date = db.DateTimeProperty(auto_now = True)
+
+class MainHandler(webapp.RequestHandler):
+   def get(self):
+      self.response.headers['Content-Type'] = 'text/javascript'
+      group = self.request.get('group')
+      if (group):
+         url = self.request.get('url')
+         callback = self.request.get('callback')
+         try:
+            if (url):
+               referrer = memcache.get(url, namespace = 'ferris')
+               if referrer is None:
+                  referrer = Referrer.get_by_key_name(url) or \
+                        Referrer(key_name = url, group = group)
+               else:
+                  self.response.headers['X-Ferris-Debug'] = 'Fetched from Memcache'
+               referrer.hits += 1
+               if referrer.hits % 10 == 0:
+                  referrer.put()
+               memcache.set(url, referrer, namespace = 'ferris')
+               self.response.headers['X-Ferris-Hits'] = referrer.hits
+               self.response.set_status(201)
+            else:
+               result = memcache.get('referrers', namespace = 'ferris')
+               if result is None:
+                  days = int(self.request.get('days') or 1)
+                  records = db.GqlQuery('select date, hits from Referrer where group = :1 \
+                        and date > :2 order by date desc', group, datetime.now() - timedelta(days = days))
+                  #records = Referrer.all()
+                  #records.filter('group', group)
+                  #records.filter('date >', datetime.now() - timedelta(days = days))
+                  #records.order('-date')
+                  #results = records.fetch(1000)
+                  referrers = []
+                  for item in records:
+                     referrers.append({
+                        'url': item.key().name(),
+                        'hits': item.hits,
+                        'date': item.date.isoformat()
+                     })
+                  result = json.dumps(referrers)
+                  memcache.set('referrers', result, 300, namespace = 'ferris')
+                  self.response.headers['X-Ferris-Debug'] = 'Fetched from Datastore'
+               else:
+                  self.response.headers['X-Ferris-Debug'] = 'Fetched from Memcache'               
+               if callback:
+                  self.response.out.write('%s(%s)' % (callback, result))
+               else:
+                  self.response.out.write(result)
+         except Exception, ex:
+            if callback:
+              self.response.out.write('%s(%s)' % (callback, \
+                  json.dumps({'status': 500, 'error': ex.__str__()})))
+            else:
+               raise
+      else:
+         self.response.set_status(400)
+      
+app = webapp.WSGIApplication([('/ferris', MainHandler)], debug = True)
+
+def main():
+   run_wsgi_app(app)
+
+if __name__ == '__main__':
+   main()
---
+++ roxy.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2011 Tobi Schäfer.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging, gzip, cStringIO, cgi
+import simplejson as json
+
+from datetime import datetime, timedelta
+
+from google.appengine.api import urlfetch
+from google.appengine.api import memcache
+
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class Resource(db.Model):
+   date = db.DateTimeProperty(auto_now_add = True)
+   etag = db.StringProperty()
+   headers = db.TextProperty()
+   content = db.TextProperty()
+   
+class HttpError(Exception):
+   def __init__(self, response):
+      self.response = response
+   def __str__(self):
+      return repr(self.response.status_code)
+   
+class MainHandler(webapp.RequestHandler):
+   def get(self):
+      self.response.headers['Content-Type'] = 'text/javascript'
+      self.response.headers['Access-Control-Allow-Origin'] = '*'
+      TTL = 60
+      url = self.request.get('url')
+      callback = self.request.get('callback')
+      result = memcache.get(url)
+      if result is None:
+         headers = {}
+         content = ''
+         try:
+            resource = Resource(key_name = url)
+            #resource = Resource.get_by_key_name(url) or \
+            #           Resource(key_name = url)
+            if resource.content is None or datetime.now() - \
+                  resource.date > timedelta(seconds = TTL):
+               response = urlfetch.fetch(url, headers = {'If-None-Match': \
+                     resource.etag, 'Accept-Encoding': 'gzip'})
+               headers.update(response.headers)
+               if headers.get('content-encoding') == 'gzip':
+                  io = cStringIO.StringIO(response.content)
+                  file = gzip.GzipFile(fileobj = io, mode = 'rb')
+                  content = file.read()
+                  file.close()
+               else:
+                  content = response.content
+               content_type = response.headers.get('content-type')
+               if content_type.startswith('text/') or \
+                     content_type.startswith('application/xml') or \
+                     content_type.startswith('application/rss+xml'):
+                  try:
+                     content = content.decode('utf-8-sig')
+                  except UnicodeDecodeError:
+                     content = content.decode('iso-8859-1')
+                  except:
+                     raise
+               else:
+                  content = content.encode('base64')
+               if response.status_code == 200:
+                  resource.content = db.Text(content)
+                  resource.etag = headers.get('etag')
+                  resource.headers = json.dumps(headers)
+                  #resource.put()
+                  self.response.headers['X-Roxy-Debug'] = 'Fetched from URL'
+               elif response.status_code != 304:
+                  raise HttpError, response
+               else:
+                  self.response.headers['X-Roxy-Debug'] = 'Fetched from Datastore'
+         except Exception, ex:
+            if callback:
+               if type(ex) == HttpError:
+                  headers['error'] = ex.response.status_code + 0 # FIXME: Without the +0 we get an array?!?
+                  headers['message'] = cgi.escape(content)
+               else:
+                  headers['error'] = 500
+                  headers['message'] = ex.__str__()
+               resource = Resource()
+               resource.headers = json.dumps(headers)
+               logging.error(resource.headers)
+               resource.date = datetime.now()
+               # Do not store errors in the database
+               #resource.put()
+            else:
+               raise
+         result = json.dumps({
+            'headers': json.loads(resource.headers),
+            'content': resource.content
+         })
+         memcache.set(url, result, TTL)
+      else:
+         self.response.headers['X-Roxy-Debug'] = 'Fetched from Memcache'
+      if callback:
+         result = '%s(%s)' % (callback, result)
+      if self.request.get('Accept-Encoding') == 'gzip':
+         self.response.headers['Content-Encoding'] = 'gzip'
+         io = cStringIO.StringIO()
+         file = gzip.GzipFile(fileobj = io, mode = 'wb')
+         file.write(result)
+         file.close()
+         result = io.getvalue()
+      self.response.out.write(result)
+
+app = webapp.WSGIApplication([('/roxy', MainHandler)], debug = True)
+
+def main():
+   run_wsgi_app(app)
+
+if __name__ == '__main__':
+   main()
