aa75690aee1a202fd8ab659dd50b65ad746f57a2
Tobi Schäfer <interface@p3k.org>
2016-03-28 19:39:55 +0200

    chg: formatted code and removed comments

--- ferris.py
+++ ferris.py
@@ -21,15 +21,13 @@ from datetime import datetime, timedelta
 from urlparse import urlparse
 
 from google.appengine.api import memcache
-
 from google.appengine.ext import db
-
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 class Referrer(db.Model):
    group = db.StringProperty()
-   hits = db.IntegerProperty(default = 0)
-   date = db.DateTimeProperty(auto_now = True)
+   hits = db.IntegerProperty(default=0)
+   date = db.DateTimeProperty(auto_now=True)
 
 class MainHandler(webapp2.RequestHandler):
    def get(self):
@@ -40,29 +38,24 @@ class MainHandler(webapp2.RequestHandler):
          callback = self.request.get('callback')
          try:
             if (url):
-               referrer = memcache.get(url, namespace = 'ferris')
+               referrer = memcache.get(url, namespace='ferris')
                if referrer is None:
                   referrer = Referrer.get_by_key_name(url) or \
-                        Referrer(key_name = url, group = group)
+                        Referrer(key_name=url, group=group)
                else:
                   self.response.headers['X-Ferris-Debug'] = 'Fetched from Memcache'
                referrer.hits += 1
                if referrer.hits % 10 == 0:
                   referrer.put()
-               memcache.set(url, referrer, namespace = 'ferris')
+               memcache.set(url, referrer, namespace='ferris')
                self.response.headers['X-Ferris-Hits'] = str(referrer.hits)
                self.response.set_status(201)
             else:
-               result = memcache.get('referrers', namespace = 'ferris')
+               result = memcache.get('referrers', namespace='ferris')
                if result is None:
                   days = int(self.request.get('days') or 1)
                   records = db.GqlQuery('select date, hits from Referrer where group = :1 \
-                        and date > :2 order by date desc', group, datetime.now() - timedelta(days = days))
-                  #records = Referrer.all()
-                  #records.filter('group', group)
-                  #records.filter('date >', datetime.now() - timedelta(days = days))
-                  #records.order('-date')
-                  #results = records.fetch(1000)
+                        and date > :2 order by date desc', group, datetime.now() - timedelta(days=days))
                   referrers = []
                   for item in records:
                      referrers.append({
@@ -71,10 +64,10 @@ class MainHandler(webapp2.RequestHandler):
                         'date': item.date.isoformat()
                      })
                   result = json.dumps(referrers)
-                  memcache.set('referrers', result, 300, namespace = 'ferris')
+                  memcache.set('referrers', result, 300, namespace='ferris')
                   self.response.headers['X-Ferris-Debug'] = 'Fetched from Datastore'
                else:
-                  self.response.headers['X-Ferris-Debug'] = 'Fetched from Memcache'               
+                  self.response.headers['X-Ferris-Debug'] = 'Fetched from Memcache'
                if callback:
                   self.response.out.write('%s(%s)' % (callback, result))
                else:
