6bfb2a959e462ceb123e5a0985fff3f5ae027b6f
Tobi Schäfer <interface@p3k.org>
2020-11-19 23:58:20 +0100

    Remove obsolete comment

--- main.py
+++ main.py
@@ -22,7 +22,4 @@ def ferris_tasks():
 
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
     app.run(host='127.0.0.1', port=8001, debug=True)
